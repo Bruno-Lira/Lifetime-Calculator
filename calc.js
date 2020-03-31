4289edcebec5cf9dda7723927f4b79568dcd670c
Bruno Lira <bruno.oli.lira@gmail.com>
2020-03-31 19:55:17 -0300

    null info

--- calc.js
+++ calc.js
@@ -1,7 +1,11 @@
 function age() {
     suaidade = document.getElementById('fname').value;
 
-    result = "Your age is " + suaidade + " years old. " + "So you have " + suaidade * 365 + " days and " + (suaidade * 365) * 24 + " hours " + ((suaidade * 365) * 24) * 60 + " minutes";
-
-    document.getElementById('result').innerHTML = result;
+    if (suaidade != '') {
+        result = "Your age is " + suaidade + " years old. " + "So you have " + suaidade * 365 + " days and " + (suaidade * 365) * 24 + " hours " + ((suaidade * 365) * 24) * 60 + " minutes";
+        document.getElementById('result').innerHTML = result;
+    }
+    else if(suaidade == ''){
+        document.getElementById('result').innerHTML = "enter your age in the text space above.";
+    }
 }
