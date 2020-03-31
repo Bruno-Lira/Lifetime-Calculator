6fec492fac5f5321a6777c15261798b01a4dc1d4
Bruno Lira <bruno.oli.lira@gmail.com>
2020-03-31 17:55:26 -0300

    translate

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
 function age() {
     suaidade = document.getElementById('fname').value;
 
-    result = "Sua idade é " + suaidade + " anos. " + "Logo você tem " + suaidade * 365 + " dias" + " e " + (suaidade * 365) * 24 + " horas " + ((suaidade * 365) * 24) * 60 + " minutos";
+    result = "Your age is " + suaidade + " years old. " + "So you have " + suaidade * 365 + " days and " + (suaidade * 365) * 24 + " hours " + ((suaidade * 365) * 24) * 60 + " minutes";
 
     document.getElementById('result').innerHTML = result;
 }
